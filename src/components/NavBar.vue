66270d476e256aba876ceae569663aa567fbb02b
ridley ye <ridleyyeh@gmail.com>
2024-05-07 17:39:26 +0800

    nav on top

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Recipe</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-        </button>
+        </button> -->
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item" v-for="link in links" :key="link.text">
@@ -29,6 +29,35 @@ const links = [
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+}
+
+nav {
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+
+    .navbar-brand {
+        font-weight: 900
+    }
+
+    div {
+
+        .navbar-nav {
+            gap: 15px;
+
+            .nav-item {
+
+
+                .nav-link {
+                    font-weight: 700;
+                }
+            }
+        }
+    }
 
 }
 </style>
