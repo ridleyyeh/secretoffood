cca34877908c6609a8e242017ce14c9b4588df28
ridley ye <ridleyyeh@gmail.com>
2024-05-07 14:14:44 +0800

    search com and fixs

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 import { ref } from 'vue';
 import NavBar from '@/components/NavBar.vue'
 import RecipeCard from '@/components/RecipeCard.vue'
-
+import SearchBar from '@/components/SearchBar.vue'
 
 const recipes = ref([
   { id: 1, title: "泰式酸辣麵", description: '簡單快速家常菜', Image: '@/assets/noodles.jpg' },
@@ -16,6 +16,7 @@ const recipes = ref([
 <template>
   <header>
     <NavBar />
+    <SearchBar />
 
   </header>
 
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, defineEmits } from 'vue';
+const searchQuery = ref('')
+const emit = defineEmits(['search'])
+const performSearch = () => {
+
+    // 實現搜索邏輯或觸發一個事件
+
+    console.log('Searching for :', searchQuery.value)
+
+    emit('search', searchQuery.value)
+
+
+}
+</script>
+
+
+<template>
+    <div class="search-bar">
+        <input v-model="searchQuery" placeholder="Search recipes..." @keyup.enter="performSearch" />
+        <button>Search</button>
+    </div>
+</template>
