e663937fd1dd6282ab62a184abd8bb46955dc390
ridley ye <ridleyyeh@gmail.com>
2024-05-07 13:33:28 +0800

    recipe complete

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card" @click="goToRecipe" style="width: 18rem;">
+        <img :src="recipe.jpg" alt="Card image cap" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">{{ recipe.title }}</h5>
+            <p class="card-text">{{ recipe.description }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const props = defineProps({
+    recipe: {
+        type: Object,
+        required: true,
+    }
+
+})
+
+
+const router = useRouter();
+
+function goToRecipe() {
+    router.push(`/recipe/${props.recipe.id}`);
+}
+</script>
